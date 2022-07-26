7fb03dabb70285e83b197e0e51b359c1b2aeb657
Matt Zinkevicius <matt.zinkevicius@hpe.com>
2022-07-26 09:55:14 -0600

    Add maven project

---
+++ myservice/src/main/java/com/mattzink/App.java
@@ -0,0 +1,13 @@
+package com.mattzink;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
